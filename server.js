f60054bb94a9b95aa7d39ee985f859dcce407fb4
Avinash Aerra <avi6105@gmail.com>
2017-07-02 21:28:03 -0700

    basic home page

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser= require('body-parser')
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.listen(3000, function() {
+    console.log('Listening on port 3000');
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html')
+});
+
+app.post('/sendMessage', function(req, res) {
+    console.log(req.body);
+});
